13540a4273573fbe575fe28aef28ea37d1e16946
Fueki <lejean.alexandre@gmail.com>
2025-04-26 10:16:32 +0200

    feat: added a 'End Turn' button

---
+++ Assets/_Scripts/Systems/EnemySystem.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using UnityEngine;
+
+public class EnemySystem : MonoBehaviour
+{
+    private void OnEnable()
+    {
+        ActionSystem.AttachPerformer<EnemyTurnGA>(EnemyTurnPerformer);
+    }
+    private void OnDisable()
+    {
+        ActionSystem.DetachPerformer<EnemyTurnGA>();
+    }
+
+    private IEnumerator EnemyTurnPerformer(EnemyTurnGA enemyTurnGA)
+    {
+        Debug.Log("Enemy turn");
+        yield return new WaitForSeconds(1f);
+    }
+}
